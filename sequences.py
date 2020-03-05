87c80da5fb525f122d71f36d1cd7a373a8b2d1e1
antonio-gor <antonio.gor@hotmail.com>
2020-03-05 14:17:49 -0800

    implement fly_straight_dammit sequence and sequence plotter

---
+++ sequences.py
@@ -0,0 +1,33 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+from math import gcd
+
+#### Utilities ####
+
+def plot(fn, start, stop):
+    range = fn(start, stop)
+    domain = np.linspace(start, stop, len(range))
+
+    plt.figure()
+    plt.plot(domain, range, 'k.', markersize=2)
+    plt.title(fn.__name__)
+    plt.grid(True)
+    plt.show()
+
+#### Sequences ####
+
+def fly_straight_dammit(start=0, stop=10):
+    def calculate(n, range_n_minus_1):
+        if gcd(n, range_n_minus_1) == 1:
+            return range_n_minus_1 + n + 1
+        else:
+            return int(range_n_minus_1 / gcd(n, range_n_minus_1))
+    
+    range = np.array([1, 1])
+    n = 2
+    while n <= stop:
+        a = calculate(n, range[n-1])
+        range = np.append(range, a)
+        n += 1
+    return range
